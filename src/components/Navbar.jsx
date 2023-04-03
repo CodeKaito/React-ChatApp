29f2808d647e1a4c0a221f45a25ceed5bb8cfc8f
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-03 19:07:00 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
+import {signOut} from "firebase/auth"
 import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
-      <span className='logo'>Teams Application</span>
-        <div className="user">
-          <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-          <span>Jerome</span>
-          <button onClick={() => signOut(auth)}>logout</button>
-        </div>
-      
+      <span className="logo">Teams</span>
+      <div className="user">
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={()=>signOut(auth)}>logout</button>
+      </div>
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import Add from "../assets/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, storage, db } from '../firebase';
-import '../style.scss'
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-import { doc,setDoc } from 'firebase/firestore';
-import { useNavigate } from 'react-router-dom';
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
@@ -18,7 +18,7 @@ const Register = () => {
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
-    
+
     try {
       //Create user
       const res = await createUserWithEmailAndPassword(auth, email, password);
@@ -45,7 +45,7 @@ const Register = () => {
 
             //create empty user chats on firestore
             await setDoc(doc(db, "userChats", res.user.uid), {});
-            navigate('/');
+            navigate("/");
           } catch (err) {
             console.log(err);
             setErr(true);
@@ -60,26 +60,29 @@ const Register = () => {
   };
 
   return (
-    <div className='formContainer'>
-        <div className='formWrapper'>
-            <span className='logo'>Chat App</span>
-            <span className='title'>Register</span>
-            <form onSubmit={handleSubmit}>
-                <input type='text' placeholder='Display name'/>
-                <input type='email' placeholder='Email'/>
-                <input type='password' placeholder='Password'/>
-                <input style={{ display: "none" }} type="file" id="file" />
-                <label htmlFor='file'>
-                    <img src='https://icons-for-free.com/iconfiles/png/512/gallery+image+landscape+mobile+museum+open+line+icon-1320183049020185924.png' alt="logo" />
-                    <span>Add an avatar</span>
-                </label>
-                <button>Sign up</button>
-                {err && <span>Something went wrong</span>}
-            </form>
-            <p>Have already an account? <span>Login</span></p>
-        </div>
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="display name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button disabled={loading}>Sign up</button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>
+          You do have an account? <Link to="/register">Login</Link>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
