0d1a76972f72e2040f2b3b1b5c43cddc31c499af
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-03 10:48:44 +0200

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { auth, storage, db } from '../firebase';
 import '../style.scss'
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { doc,setDoc } from 'firebase/firestore';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Register = () => {
   const [err, setErr] = useState(false);
